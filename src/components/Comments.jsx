3970c9a68e3458a1c210d141cac058bb0e44f66b
AVIN4SH <savinashkumarsingh619@gmail.com>
2025-06-12 10:30:41 +0530

    04. Added Link to Source Code

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,4 +1,4 @@
-import { ChevronUp } from "lucide-react";
+import { ChevronUp, Github } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 const commentsData = [
@@ -89,7 +89,7 @@ function CommentItem({ comment, depth = 0 }) {
 
 export default function Comments() {
   return (
-    <div className="max-w-4xl mx-auto border border-black rounded-lg mt-20">
+    <div className="max-w-4xl mx-auto border border-black rounded-lg mt-10 sm:mt-12 md:mt-16 lg:mt-20  max-md:w-[85%] max-lg:w-[90%]">
       <div className="bg-white rounded-lg shadow-sm border border-gray-200 p-6">
         <div className="space-y-1">
           {commentsData.map((comment) => (
@@ -97,6 +97,32 @@ export default function Comments() {
           ))}
         </div>
       </div>
+      <a href="https://github.com/AVIN4SH/comments-thread" target="_blank">
+        <Button
+          variant="primary"
+          size="lg"
+          className="h-12 w-full p-4 bg-black rounded-sm text-white hover:bg-black/90 transition-colors"
+        >
+          <span className="border border-white rounded-full p-1 scale-125">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="lucide lucide-github-icon lucide-github"
+            >
+              <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+              <path d="M9 18c-4.51 2-5-2-7-2" />
+            </svg>
+          </span>
+          Source Code
+        </Button>
+      </a>
     </div>
   );
 }
